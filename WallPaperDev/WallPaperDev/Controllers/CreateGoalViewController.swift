2018d6087bf74a43f9c45eb97cdb59321c1c2b8a
Jackson Ho <mintri1199@gmail.com>
2019-09-14 18:04:35 -0700

    [Modified] modified to satisfy linter

--- WallPaperDev/WallPaperDev/Controllers/CreateGoalViewController.swift
+++ WallPaperDev/WallPaperDev/Controllers/CreateGoalViewController.swift
@@ -26,4 +26,3 @@ class CreateGoalViewController: UIViewController {
         navigationController?.navigationBar.isHidden = false
     }
 }
-
--- WallPaperDev/WallPaperDevTests/WallPaperDevTests.swift
+++ WallPaperDev/WallPaperDevTests/WallPaperDevTests.swift
@@ -13,10 +13,12 @@ class WallPaperDevTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
     func testExample() {
@@ -30,5 +32,4 @@ class WallPaperDevTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
