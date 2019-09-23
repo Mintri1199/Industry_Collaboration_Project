8d57f1ff067782e08ae6ca0c3a6441af203fd8a4
Xi Ouyang <xisouyang@gmail.com>
2019-09-23 14:54:22 -0700

    [refactor] refactored code to add button to home page

--- WallPaperDev/WallPaperDev/Controllers/HomeViewController.swift
+++ WallPaperDev/WallPaperDev/Controllers/HomeViewController.swift
@@ -22,23 +22,33 @@ class HomeViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         initViews()
+        initButton()
     }
     
     private func initViews() {
         let titleViewFrame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height / 2.3)
         let emptyStateViewFrame = CGRect(x: 0, y: view.bounds.height * 0.38, width: view.frame.width * 0.9, height: view.frame.height / 2.3)
-        let addButtonFrame = CGRect(x: view.bounds.width * 0.75, y: view.bounds.height * 0.85, width: view.bounds.width / 5, height: view.bounds.width / 5)
+       
         titleView.frame = titleViewFrame
         emptyStateView.frame = emptyStateViewFrame
         emptyStateView.center.x = view.center.x
-        addButton.frame = addButtonFrame
         view.addSubview(titleView)
         view.addSubview(emptyStateView)
+    }
+    
+    private func initButton() {
+        let addButtonFrame = CGRect(x: view.bounds.width * 0.75, y: view.bounds.height * 0.85, width: view.bounds.width / 5, height: view.bounds.width / 5)
+        addButton.frame = addButtonFrame
+        addButton.addTarget(self, action: #selector(addTapped), for: .touchUpInside)
         view.addSubview(addButton)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.isHidden = true
+    }
+    
+    @objc func addTapped() {
+        print("tapped")
     }
 }
