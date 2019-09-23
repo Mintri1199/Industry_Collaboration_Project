b0c0f269f51b2258de52c25a5f4478d51680bdad
Xi Ouyang <xisouyang@gmail.com>
2019-09-23 12:28:44 -0700

    [add] add button UI to home page

--- WallPaperDev/WallPaperDev/Controllers/HomeViewController.swift
+++ WallPaperDev/WallPaperDev/Controllers/HomeViewController.swift
@@ -12,21 +12,29 @@ class HomeViewController: UIViewController {
     
     let titleView = HomeBackgroundView(frame: .zero)
     let emptyStateView = EmptyStateView(frame: .zero)
+    let addButton = AddButton(frame: .zero)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.9052448869, green: 0.8998637795, blue: 0.9093814492, alpha: 1)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         initViews()
     }
     
     private func initViews() {
         let titleViewFrame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height / 2.3)
-        let emptyStateViewFrame = CGRect(x: 0, y: view.bounds.height * 0.38, width: view.frame.width * 0.9, height: view.frame.height / 2)
+        let emptyStateViewFrame = CGRect(x: 0, y: view.bounds.height * 0.38, width: view.frame.width * 0.9, height: view.frame.height / 2.3)
+        let addButtonFrame = CGRect(x: view.bounds.width * 0.75, y: view.bounds.height * 0.85, width: view.bounds.width / 5, height: view.bounds.width / 5)
         titleView.frame = titleViewFrame
         emptyStateView.frame = emptyStateViewFrame
         emptyStateView.center.x = view.center.x
+        addButton.frame = addButtonFrame
         view.addSubview(titleView)
         view.addSubview(emptyStateView)
+        view.addSubview(addButton)
     }
     
     override func viewWillAppear(_ animated: Bool) {
---
+++ WallPaperDev/WallPaperDev/Views/AddButton.swift
@@ -0,0 +1,34 @@
+//
+//  AddButton.swift
+//  WallPaperDev
+//
+//  Created by Stephen Ouyang on 9/23/19.
+//  Copyright © 2019 Stephen Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class AddButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override func didMoveToSuperview() {
+        configButton()
+    }
+    
+    private func configButton() {
+        self.setTitle("Add", for: .normal)
+        self.backgroundColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
+        self.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        self.setTitleColor(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1), for: .highlighted)
+        self.titleLabel?.font = UIFont(name: "Avenir-Heavy", size: 20)
+        
+        self.layer.cornerRadius = self.bounds.width / 2
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
