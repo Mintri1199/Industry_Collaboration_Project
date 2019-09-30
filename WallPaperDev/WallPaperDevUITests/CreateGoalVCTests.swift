37086a3c7f3e2c7069a0ec08f0de26364031c158
j-n4m4573 <jg_220@yahoo.com>
2019-09-30 14:34:15 -0700

    [Added] adds CreateGoalVCTest class

---
+++ WallPaperDev/WallPaperDevUITests/CreateGoalVCTests.swift
@@ -0,0 +1,16 @@
+//
+//  CreateGoalVCTests.swift
+//  WallPaperDevUITests
+//
+//  Created by Jamar Gibbs on 9/30/19.
+//  Copyright © 2019 Stephen Ouyang. All rights reserved.
+//
+
+import XCTest
+
+class CreateGoalVCTests: XCTestCase {
+// ----> Arrange, Act, Assert
+    
+    
+    
+}
