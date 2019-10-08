a2ddbe9cd1107b562761a2be73d789d4d6dc229d
Jackson Ho <mintri1199@gmail.com>
2019-10-08 11:17:54 -0700

    [Modified] resolve linter some linter issues

--- WallPaperDev/WallPaperDev/Controllers/LivePreviewViewController.swift
+++ WallPaperDev/WallPaperDev/Controllers/LivePreviewViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class LivePreviewViewController: UIViewController {
-
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     lazy var livePreview: UIImageView = {
         let view = UIImageView(frame: UIScreen.main.bounds)
         view.contentMode = .scaleAspectFit
@@ -18,22 +22,18 @@ class LivePreviewViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(livePreview)
-        livePreview.backgroundColor = .brown
-        
-        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(dismissPreview))
-        swipeDown.direction = .down
-        
-        view.addGestureRecognizer(swipeDown)
-        
-        
-    }
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
+        setupLivePreview()
     }
     
     @objc private func dismissPreview() {
         dismiss(animated: true, completion: nil)
     }
     
+    private func setupLivePreview() {
+        view.addSubview(livePreview)
+        livePreview.backgroundColor = .brown
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(dismissPreview))
+        swipeDown.direction = .down
+        view.addGestureRecognizer(swipeDown)
+    }
 }
