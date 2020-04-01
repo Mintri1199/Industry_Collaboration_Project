a474ebe6a4db4fe12656a4b4ac4e3b8810cd6a34
Jackson Ho <mintri1199@gmail.com>
2020-03-31 21:51:54 -0400

    [Modified] Add theme images tests

--- WallPaperDev/KabegamiTests/CoreDataTests.swift
+++ WallPaperDev/KabegamiTests/CoreDataTests.swift
@@ -14,6 +14,7 @@ import XCTest
 class CoreDataTests: XCTestCase {
   var manager: CoreDataStack!
   
+  // I think the problem might be here
   lazy var managedObjectModel: NSManagedObjectModel = {
     let managedObjectModel = NSManagedObjectModel.mergedModel(from: [Bundle(for: type(of: self))] )!
     return managedObjectModel
--- WallPaperDev/KabegamiTests/KabegamiTests.swift
+++ WallPaperDev/KabegamiTests/KabegamiTests.swift
@@ -11,10 +11,58 @@ import XCTest
 @testable import Kabegami
 
 class KabegamiTests: XCTestCase {
-
+  
   private let themeManager = ApplicationDependency.manager
-
+  
   override func setUpWithError() throws {}
-
+  
   override func tearDownWithError() throws {}
+  
+  func testImagesExistence() {
+    var bundlePhoto: UIImage?
+    bundlePhoto = UIImage(named: "unsplash")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.unsplashLogo, bundlePhoto)
+    
+    bundlePhoto = UIImage(named: "background_preview_camera")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.backgroundPreviewCamera, bundlePhoto)
+    
+    bundlePhoto = UIImage(named: "background_preview_flashlight")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.backgroundPreviewFlashlight, bundlePhoto)
+    
+    bundlePhoto = UIImage(named: "tutorial_example_1")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.tutorialExample1, bundlePhoto)
+    
+    bundlePhoto = UIImage(named: "tutorial_finger")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.tutorialFinger, bundlePhoto)
+    
+    bundlePhoto = UIImage(named: "tutorial_goal_text_example")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.tutorialGoalTextExample, bundlePhoto)
+    
+    bundlePhoto = UIImage(named: "tutorial_phone")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.tutorialPhone, bundlePhoto)
+    
+    bundlePhoto = UIImage(named: "tutorial_todo_banner")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.tutorialTodoBanner, bundlePhoto)
+    
+    bundlePhoto = UIImage(named: "tutorial_welcome_banner")
+    
+    XCTAssertNotNil(bundlePhoto)
+    XCTAssertEqual(themeManager.currentTheme.imageAssets.tutorialWelcomeBanner, bundlePhoto)
+  }
 }
