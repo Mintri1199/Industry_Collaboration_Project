d3232b831bce009a8f148ec93fdef89eebdf331e
Jackson Ho <mintri1199@gmail.com>
2020-05-19 04:51:05 -0400

    [Modified] finished configuring license cells

--- WallPaperDev/WallPaperDev/Views/Controllers/AboutViewController.swift
+++ WallPaperDev/WallPaperDev/Views/Controllers/AboutViewController.swift
@@ -7,10 +7,7 @@
 
 import UIKit
 import SafariServices
-// In this view controller I want to have the following functionalities
-// * Deep linking to developers github profiles
-// * Credits for borrowed assets
-// *
+
 
 class AboutViewController: UIViewController {
   enum Section: Int, CaseIterable {
@@ -46,11 +43,8 @@ class AboutViewController: UIViewController {
     }
   }
   
-  //  let tableView = UITableView(frame: .zero, style: .insetGrouped)
   private var collectionView: UICollectionView!
   private var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
-  //  private var dataSource: UITableViewDiffableDataSource<Section, Item>!
-  //  private var currentSnapshot: NSDiffableDataSourceSnapshot<Section, Item>!
   private let viewModel = AboutViewModel()
   
   static let reuseIdentifier = "reuse-identifier"
@@ -67,7 +61,6 @@ class AboutViewController: UIViewController {
 // MARK: UICollectionViewCompositional Layout
 extension AboutViewController {
   private func createLayout() -> UICollectionViewLayout {
-    
     let compositionLayout = UICollectionViewCompositionalLayout { (sectionIndex: Int, _ : NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
       guard let sectionLayoutKind = Section(rawValue: sectionIndex) else {
         return nil
@@ -77,7 +70,7 @@ extension AboutViewController {
       let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                             heightDimension: columns == 1 ? .estimated(250) : .fractionalHeight(1.0))
       
-      let item = NSCollectionLayoutItem(layoutSize: itemSize)      
+      let item = NSCollectionLayoutItem(layoutSize: itemSize)
       let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
                                              heightDimension: .estimated(175))
       
@@ -90,33 +83,6 @@ extension AboutViewController {
       
       return section
     }
-    
-    let profileSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5),
-                                             heightDimension: .fractionalHeight(1.0))
-    
-    let profileItem = NSCollectionLayoutItem(layoutSize: profileSize)
-    
-    profileItem.contentInsets = NSDirectionalEdgeInsets(top: 15, leading: 20, bottom: 0, trailing: 20)
-    let profileGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                  heightDimension: .estimated(175))
-    let profileGroup = NSCollectionLayoutGroup.horizontal(layoutSize: profileGroupSize, subitems: [profileItem])
-    
-    let profileSection = NSCollectionLayoutSection(group: profileGroup)
-    
-    let creditSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                            heightDimension: .fractionalHeight(1.0))
-    
-    let creditItem = NSCollectionLayoutItem(layoutSize: creditSize)
-    creditItem.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
-    
-    let creditGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
-                                                 heightDimension: .estimated(200))
-    
-    let creditGroup = NSCollectionLayoutGroup.vertical(layoutSize: creditGroupSize, subitems: [creditItem])
-    
-    let creditSection = NSCollectionLayoutSection(group: creditGroup)
-    
-    let layout = UICollectionViewCompositionalLayout(section: profileSection)
     
     return compositionLayout
   }
@@ -154,19 +120,28 @@ extension AboutViewController {
         }
         cell.prepare(text: string)
         return cell
-      default:
-        return nil
+      
+        case let .library(title, license):
+          guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: LibraryCollectionViewCell.id, for: indexPath) as? LibraryCollectionViewCell else {
+            return nil
+          }
+          cell.prepare(title: title, bodyText: license)
+          return cell
       }
     })
     
     var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
     
-    snapshot.appendSections([.profiles, .feedback])
+    snapshot.appendSections([.profiles, .feedback, .libraries])
     let profileItems = viewModel.devProfiles.compactMap { Item(type: .profile(dev: $0)) }
     snapshot.appendItems(profileItems, toSection: .profiles)
     
     let feedbackItem = Item(type: .feedback(text: viewModel.contructFeedbackText()))
     snapshot.appendItems([feedbackItem], toSection: .feedback)
+    
+    let libraryItems = viewModel.getLicenseMaterials().compactMap { Item(type: .library(title: $0, license: $1)) }
+    snapshot.appendItems(libraryItems, toSection: .libraries)
+    
     dataSource.apply(snapshot, animatingDifferences: false)
   }
 }
@@ -175,59 +150,6 @@ extension AboutViewController {
 extension AboutViewController {
   
   func configureColletionView() {}
-  
-  //  func configureTableView() {
-  //    self.view.addSubview(tableView)
-  //    tableView.translatesAutoresizingMaskIntoConstraints = false
-  //    tableView.estimatedRowHeight = 200
-  //    tableView.separatorStyle = .none
-  //    tableView.bounces = false
-  //    tableView.rowHeight = UITableView.automaticDimension
-  //    NSLayoutConstraint.activate([
-  //      tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-  //      tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-  //      tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-  //      tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
-  //    ])
-  //    tableView.register(AboutTableViewCell.self, forCellReuseIdentifier: AboutViewController.reuseIdentifier)
-  //  }
-  //
-  //  private func configureDataSource() {
-  //    self.dataSource = UITableViewDiffableDataSource<Section, Item>(tableView: tableView) { [weak self] (tableView: UITableView, _ : IndexPath, item: Item) -> UITableViewCell? in
-  //      guard let self = self, let cell = tableView.dequeueReusableCell(withIdentifier: AboutViewController.reuseIdentifier) as? AboutTableViewCell else {
-  //        return nil
-  //      }
-  //
-  //      tableView.sectionHeaderHeight = 0
-  //
-  //      switch item.type {
-  //      case .profile:
-  //        // FLag: Color
-  //        cell.backgroundColor = .yellow
-  //        cell.prepareProfiles { collectionView in
-  //          collectionView.delegate = self
-  //        }
-  //      case .credit:
-  //        cell.prepare(type: .credits(text: self.viewModel.contructFeedbackText()))
-  //      case .library:
-  //        cell.prepare(type: .library(title: item.name ?? "", license: item.bodyText ?? ""))
-  //      }
-  //
-  //      return cell
-  //    }
-  //  }
-  //
-  //  private func updateUI() {
-  //    //
-  //    currentSnapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-  //    let creditItem = Item(type: .credit)
-  //    let licenseItems = viewModel.getLicenseMaterials().map { Item(type: .library(title: $0.0, license: $0.1)) }
-  //    currentSnapshot.appendSections([.credits, .libraries])
-  //    currentSnapshot.appendItems([creditItem], toSection: .credits)
-  //    currentSnapshot.appendItems(licenseItems, toSection: .libraries)
-  ////
-  //    self.dataSource.apply(currentSnapshot, animatingDifferences: true)
-  //  }
 }
 
 extension AboutViewController: UICollectionViewDelegate {
